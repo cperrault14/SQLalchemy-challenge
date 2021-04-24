a3e3f8396e18bf9e279b64c2dd672e4c1aea4e6e
Christina <cperrault14@users.noreply.github.com>
2021-04-24 10:45:21 -0400

    Modified name of file app.py

--- app.py
+++ app.py
@@ -1,284 +1,108 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#Dependencies\n",
-    "import numpy as np\n",
-    "from sqlalchemy.ext.automap import automap_base\n",
-    "from sqlalchemy.orm import Session\n",
-    "from sqlalchemy import create_engine, func\n",
-    "from flask import Flask, jsonify\n",
-    "from gevent.pywsgi import WSGIServer\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# create engine to hawaii.sqlite\n",
-    "engine = create_engine(\"sqlite:///Resources/hawaii.sqlite\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# reflect an existing database into a new model\n",
-    "Base = automap_base()\n",
-    "# reflect the tables\n",
-    "Base.prepare(engine, reflect=True)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "['measurement', 'station']"
-      ]
-     },
-     "execution_count": 4,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "# View all of the classes that automap found\n",
-    "Base.classes.keys()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Save references to each table\n",
-    "Measurement = Base.classes.measurement\n",
-    "Station = Base.classes.station"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Create our session (link) from Python to the DB\n",
-    "session = Session(engine)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#Flask setup\n",
-    "app = Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#Flask Route\n",
-    "#Home Page\n",
-    "#List all routes that are available\n",
-    "\n",
-    "@app.route(\"/\")\n",
-    "def home():\n",
-    "    print(\"Server active for Home Page\")\n",
-    "    return (\n",
-    "        f\"Welcome to the SQL-Alchemy APP API!<br/>\"\n",
-    "        f\"Available Routes:<br/>\"\n",
-    "        f\"/api/v1.0/precipitation<br/>\"\n",
-    "        f\"/api/v1.0/stations<br/>\"\n",
-    "        f\"/api/v1.0/tobs<br/>\"\n",
-    "        f\"/api/v1.0/<start><br>\"\n",
-    "        f\"/api/v1.0<start>/<end><br>\"\n",
-    "    ) \n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# /api/v1.0/precipitation\n",
-    "# Convert the query results to a dictionary using date as the key and prcp as the value.\n",
-    "# Return the JSON representation of your dictionary.\n",
-    "\n",
-    "@app.route(\"/api/v1.0/precipitation\")\n",
-    "def precipitation():\n",
-    "    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()\n",
-    "    all_precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').all()\n",
-    "    precip_dict = dict(all_precip)\n",
-    "    return jsonify(precip_dict)\n",
-    "\n",
-    "session.close()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 10,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# /api/v1.0/stations\n",
-    "# Return a JSON list of stations from the dataset.\n",
-    "@app.route(\"/api/v1.0/stations\")\n",
-    "def stations():\n",
-    "    all_stations = (session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all())\n",
-    "    list_stations = list(np.ravel(all_stations))\n",
-    "    return jsonify(stations_list)\n",
-    "\n",
-    "session.close()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# /api/v1.0/tobs\n",
-    "# Query the dates and temperature observations of the most active station for the last year of data.\n",
-    "# Return a JSON list of temperature observations (TOBS) for the previous year.\n",
-    "@app.route(\"/api/v1.0/tobs\")\n",
-    "def tobs():\n",
-    "    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()\n",
-    "    all_tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').all()\n",
-    "    list_tobs = list(all_tobs)\n",
-    "    return jsonify(list_tobs)\n",
-    "\n",
-    "session.close()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 12,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# /api/v1.0/<start> \n",
-    "# /api/v1.0/<start>/<end>\n",
-    "# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.\n",
-    "# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.\n",
-    "# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.\n",
-    "@app.route(\"/api/v1.0/<start>\")\n",
-    "def start(start):\n",
-    "    all_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()\n",
-    "    list_start = list(all_start)\n",
-    "    return jsonify(list_start)\n",
-    "\n",
-    "session.close()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Start_End\n",
-    "@app.route(\"/api/v1.0/<start>/<end>\")\n",
-    "def start_end(start, end):\n",
-    "    start_end = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()\n",
-    "    list_start_end = list(start_end)\n",
-    "    return jsonify(list_start_end)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 15,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " * Serving Flask app \"__main__\" (lazy loading)\n",
-      " * Environment: production\n",
-      "   WARNING: This is a development server. Do not use it in a production deployment.\n",
-      "   Use a production WSGI server instead.\n",
-      " * Debug mode: on\n"
-     ]
-    },
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      " * Restarting with windowsapi reloader\n"
-     ]
-    },
-    {
-     "ename": "SystemExit",
-     "evalue": "1",
-     "output_type": "error",
-     "traceback": [
-      "An exception has occurred, use %tb to see the full traceback.\n",
-      "\u001b[1;31mSystemExit\u001b[0m\u001b[1;31m:\u001b[0m 1\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Wrap it up\n",
-    "if __name__ == '__main__':\n",
-    "    app.run(debug=True)\n",
-    "    \n",
-    "http_server = WSGIServer(('', 5000), app)\n",
-    "http_server.serve_forever()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python [conda env:root] *",
-   "language": "python",
-   "name": "conda-root-py"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#Dependencies
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+from gevent.pywsgi import WSGIServer
+
+
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# View all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#Flask setup
+app = Flask(__name__)
+
+#Flask Route
+#Home Page
+#List all routes that are available
+
+@app.route("/")
+def home():
+    print("Server active for Home Page")
+    return (
+        f"Welcome to the SQL-Alchemy APP API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br>"
+        f"/api/v1.0<start>/<end><br>"
+    ) 
+
+# /api/v1.0/precipitation
+# Convert the query results to a dictionary using date as the key and prcp as the value.
+# Return the JSON representation of your dictionary.
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    all_precip = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').all()
+    precip_dict = dict(all_precip)
+    return jsonify(precip_dict)
+
+session.close()
+
+# /api/v1.0/stations
+# Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def stations():
+    all_stations = (session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all())
+    list_stations = list(np.ravel(all_stations))
+    return jsonify(stations_list)
+
+session.close()
+
+# /api/v1.0/tobs
+# Query the dates and temperature observations of the most active station for the last year of data.
+# Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    most_recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    all_tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').all()
+    list_tobs = list(all_tobs)
+    return jsonify(list_tobs)
+
+session.close()
+
+# /api/v1.0/<start> 
+# /api/v1.0/<start>/<end>
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+@app.route("/api/v1.0/<start>")
+def start(start):
+    all_start = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).group_by(Measurement.date).all()
+    list_start = list(all_start)
+    return jsonify(list_start)
+
+session.close()
+
+# Start_End
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    start_end = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
+    list_start_end = list(start_end)
+    return jsonify(list_start_end)
+
+# Wrap it up
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+http_server = WSGIServer(('', 5000), app)
+http_server.serve_forever()
